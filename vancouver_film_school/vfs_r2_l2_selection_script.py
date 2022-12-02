dd1fabe10ce602afacedb8219478d1615812ee92
srweise <114253889+srweise@users.noreply.github.com>
2022-12-01 21:24:20 -0600

    Update vfs_r2_l2_selection_script.py

--- vancouver_film_school/vfs_r2_l2_selection_script.py
+++ vancouver_film_school/vfs_r2_l2_selection_script.py
@@ -1,6 +1,11 @@
 import maya.cmds as cmds #Imports Maya API
 
 selection = cmds.ls(selection=True) # stores selection in a variable
+# If you use a list comprehension you can fit everything into 2 lines of code instead of 5 and its more pythonic.
+# Example:
+# my_list = [i for i in selection if "end" not in i]
+# cmds.select(my_list)
+
 my_list = [] # creates an empty list
 
 for obj in selection: # Goes through objects in selection
